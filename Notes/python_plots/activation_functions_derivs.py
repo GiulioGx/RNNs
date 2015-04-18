8134c42d6c3ca9d4ebd0224a2ab75d6e05084b2a
Giulio <giulio.galvan@gmail.com>
2015-04-18 17:24:11 +0200

    plot tanh, expressivness++, activation funs++

--- Notes/python_plots/activation_functions_derivs.py
+++ Notes/python_plots/activation_functions_derivs.py
@@ -2,14 +2,33 @@ from pylab import *
 import numpy
 
 
-ylim([-0.2,1])
 linewidth= 2.0;
 x_start=-4;
 x_end = 5;
 xlim(x_start,x_end-1);
 x = numpy.arange(x_start,x_end,0.01)
 
-##sigmoid
+##tanh
+ylim([-1.2,1.2])
+y_tanh = (numpy.exp(x)-numpy.exp(-x))/(numpy.exp(x)+numpy.exp(-x));
+y1_tanh = 1-y_tanh*y_tanh;
+
+
+
+line_3, = plot(x, y_tanh,color='g',linewidth=linewidth);
+line_31, = plot(x, y1_tanh,color='y',linewidth=linewidth);
+
+legend([line_3, line_31],['tanh','derivative'],shadow=True, fancybox=True,loc=0)
+
+xlabel('x')
+ylabel('y')
+grid(True)
+yticks(numpy.arange(-1.2,1.2,0.2))
+xticks(numpy.arange(x_start,x_end,1))
+
+#sigmoid
+figure();
+ylim([-0.2,1])
 y_sigmoid = 1/(1+numpy.exp(-x));
 y1_sigmoid = y_sigmoid * (1-y_sigmoid);
 
@@ -25,6 +44,7 @@ ylabel('y')
 grid(True)
 yticks(numpy.arange(-0.2,1,0.2))
 xticks(numpy.arange(x_start,x_end,1))
+
 
 #ReLU
 figure();
