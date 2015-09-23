f53eabf2502b0cb7c4b60909c7c980f228eebabc
Giulio <giulio.galvan@gmail.com>
2015-09-23 09:24:54 +0100

    a bit of code

--- sources/RNN.py
+++ sources/RNN.py
@@ -1,39 +1,60 @@
 import theano as T
 import theano.tensor as TT
+import numpy
+from configs import Configs
 
 __author__ = 'giulio'
 
 
 class RNN:
-    def __init__(self, task, activation_fnc, n_hidden):
+    def __init__(self, task, activation_fnc, output_fnc, n_hidden):
 
+        # topology
         self.__n_hidden = n_hidden
-        self.__task = task
+        self.__n_in = task.n_in
+        self.__n_out = task.n_out
+
+        # activation fnc
         self.__activation_fnc = activation_fnc
 
-        W_uh = numpy.asarray(
-            self.__rng.normal(size=(self.__nin, self.__n_hidden), scale=.01, loc=.0), dtype=self.__floatX)
-        W_hh = numpy.asarray(
-            self.__rng.normal(size=(self.__n_hidden, self.__n_hidden), scale=.01, loc=.0), dtype=self.__floatX)
-        W_hy = numpy.asarray(
-            self.__rng.normal(size=(self.__n_hidden, self.__nout), scale=.01, loc=0.0), dtype=self.__floatX)
-        b_hh = numpy.zeros((self.__n_hidden,), dtype=self.__floatX)
-        b_hy = numpy.zeros((self.__nout,), dtype=self.__floatX)
+        #output_fnc
+        self.__output_fnc = output_fnc
 
-        self.__W_uh = T.shared(W_uh, 'W_uh')
-        self.__W_hh = T.shared(W_hh, 'W_hh')
-        self.__W_hy = T.shared(W_hy, 'W_hy')
-        self.__b_hh = T.shared(b_hh, 'b_hh')
-        self.__b_hy = T.shared(b_hy, 'b_hy')
 
-    def __get_h(self, h0_tm1, u):
+        # task
+        self.__task = task
 
-        def h_t(u_t, h_tm1, W_hh, W_uh, W_hy):
-            return self.__activation_fnc(TT.dot(h_tm1, W_hh) + TT.dot(u_t, W_uh) + self.__b_hh)
+        # init weight matrices
+        W_in = numpy.asarray(
+            self.__rng.normal(size=(self.__n_hidden, self.__nin), scale=.01, loc=.0), dtype=Configs.floatType)
+        W_rec = numpy.asarray(
+            self.__rng.normal(size=(self.__n_hidden, self.__n_hidden), scale=.01, loc=.0), dtype=Configs.floatType)
+        W_out = numpy.asarray(
+            self.__rng.normal(size=(self.__nout, self.__n_hidden), scale=.01, loc=0.0), dtype=Configs.floatType)
+
+        # init biases
+        b_rec = numpy.zeros((self.__n_hidden, 1), Configs.floatType)
+        b_out = numpy.zeros((self.__nout, 1), Configs.floatType)
+
+        self.__W_in = T.shared(W_in, 'W_in')
+        self.__W_rec = T.shared(W_rec, 'W_rec')
+        self.__W_out = T.shared(W_out, 'W_out')
+        self.__b_rec = T.shared(b_rec, 'b_rec')
+        self.__b_out = T.shared(b_out, 'b_out')
+
+    def __h(self, h0_tm1, u):
+
+        def h_t(u_t, h_tm1):
+            return self.__activation_fnc(TT.dot(self.__W_rec, h_tm1) + TT.dot(self.__W_in, u_t) + self.__b_rec)
 
         h, _ = T.scan(h_t, sequences=u,
                       outputs_info=[h0_tm1],
-                      non_sequences=[self.__W_hh, self.__W_uh, self.__W_hy],
-                      name='recurrent_fn',
-                      mode=theano.Mode(linker='cvm'))
+                      non_sequences=[self.__W_rec, self.__W_in],
+                      name='h_t',
+                      mode=T.Mode(linker='cvm'))
         return h
+
+    def __y(self, h):
+
+        return self.__output_fnc(self.__W_out, TT.dot(h[-1]) + self.__b_out)
+
