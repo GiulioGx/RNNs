6816f1db008c15b003dbee96e77bc0a58b9df463
Giulio <giulio.galvan@gmail.com>
2015-10-27 19:31:30 +0000

    equiangular

--- sources/run.py
+++ sources/run.py
@@ -8,6 +8,8 @@ from NetTrainer import NetTrainer
 from TrainingRule import TrainingRule
 from learningRule.ArmijoStep import ArmijoStep
 from learningRule.ConstantNormalizedStep import ConstantNormalizedStep
+from learningRule.ConstantStep import ConstantStep
+from penalty.ConstantPenalty import ConstantPenalty
 from tasks.AdditionTask import AdditionTask
 from Configs import Configs
 from penalty.NullPenalty import NullPenalty
@@ -35,18 +37,18 @@ activation_fnc = Tanh()
 output_fnc = RNN.last_linear_fnc
 loss_fnc = NetTrainer.squared_error
 #penalty = MeanPenalty()
-#penalty = ConstantPenalty()
-penalty = NullPenalty()
+penalty = ConstantPenalty()
+#penalty = NullPenalty()
 dir_rule = AntiGradient()
 #dir_rule = AntiGradientWithPenalty(penalty, 1) #0.001
 #dir_rule = MidAnglePenaltyDirection(penalty)
 #dir_rule = FrozenGradient(penalty)
 #dir_rule = SepareteGradient()
 #lr_rule = WRecNormalizedStep(0.0001) #0.01
-#lr_rule = ConstantStep(0.01) #0.01
+lr_rule = ConstantStep(0.001) #0.01
 #lr_rule = ConstantNormalizedStep(0.001) #0.01
-lr_rule = ArmijoStep(alpha=0.1, beta=0.1, init_step=1, max_steps=50)
-obj_fnc = ObjectiveFunction(loss_fnc, penalty, 14)
+#lr_rule = ArmijoStep(alpha=0.1, beta=0.1, init_step=1, max_steps=50)
+obj_fnc = ObjectiveFunction(loss_fnc, penalty, 0.5)
 train_rule = TrainingRule(dir_rule, lr_rule)
 trainer = NetTrainer(train_rule, obj_fnc)
 net = trainer.train(task, activation_fnc, output_fnc, n_hidden, seed)
