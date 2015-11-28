e70627c20130395a79020f4035febf1243f5bf7e
Giulio <giulio.galvan@gmail.com>
2015-11-28 10:11:33 +0000

    params

--- sources/runAddition.py
+++ sources/runAddition.py
@@ -49,11 +49,11 @@ print(separator)
 
 # setup
 seed = 13
-task = MultiplicationTask(144, seed)
+task = XorTask(144, seed)
 n_hidden = 50
 activation_fnc = Tanh()
-output_fnc = RNN.linear_fnc
-loss_fnc = SquaredError()
+output_fnc = RNN.logistic
+loss_fnc = CrossEntropy()
 out_dir = Configs.output_dir+str(task)
 
 # init strategy
@@ -87,7 +87,7 @@ dir_rule = CombinedGradients(combining_rule)
 # learning step rule
 # lr_rule = WRecNormalizedStep(0.0001) #0.01
 #lr_rule = ConstantNormalizedStep(0.001)  # 0.01
-lr_rule = GradientClipping(lr_value=0.03, clip_thr=0.1)  # 0.01
+lr_rule = GradientClipping(lr_value=0.01, clip_thr=0.1)  # 0.01
 #lr_rule = ArmijoStep(alpha=0.5, beta=0.5, init_step=1, max_steps=50)
 
 obj_fnc = ObjectiveFunction(loss_fnc)
