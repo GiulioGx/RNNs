73ed3c3ed84fb9c51c3165245b998faa69b65c22
Giulio <giulio.galvan@gmail.com>
2015-11-30 21:09:31 +0000

    parmas

--- sources/runAddition.py
+++ sources/runAddition.py
@@ -88,7 +88,7 @@ dir_rule = CombinedGradients(combining_rule)
 # learning step rule
 # lr_rule = WRecNormalizedStep(0.0001) #0.01
 #lr_rule = ConstantNormalizedStep(0.001)  # 0.01
-lr_rule = GradientClipping(lr_value=0.01, clip_thr=1)  # 0.01
+lr_rule = GradientClipping(lr_value=0.03, clip_thr=0.1)  # 0.01
 #lr_rule = ArmijoStep(alpha=0.5, beta=0.5, init_step=1, max_steps=50)
 
 obj_fnc = ObjectiveFunction(loss_fnc)
