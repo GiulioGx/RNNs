1aaa7eeb37aa761c2265dff721e5ee41ab00d41e
Giulio <giulio.galvan@gmail.com>
2015-12-17 16:26:39 +0000

    params

--- sources/runAddition3.py
+++ sources/runAddition3.py
@@ -98,7 +98,7 @@ init_strategies = {'W_rec': RandomConnectionsInit(n_connections_per_unit=n_conns
 # dir_rule = SepareteGradient()
 
 #combining_rule = OnesCombination(normalize_components=False)
-combining_rule = SimplexCombination(normalize_components=True)
+combining_rule = OnesCombination(normalize_components=True)
 #combining_rule = SimpleSum()
 #combining_rule = EquiangularCombination()
 #combining_rule = DropoutCombination(drop_rate=0.8)
@@ -122,7 +122,7 @@ update_rule = SimpleUdpate()
 train_rule = TrainingRule(dir_rule, lr_rule, update_rule)
 
 trainer = NetTrainer(train_rule, obj_fnc, output_dir=out_dir, max_it=10 ** 10,
-                     check_freq=200, bacth_size=100)
+                     check_freq=200, bacth_size=10000)
 
 #dataset = Dataset.no_valid_dataset_from_task(size=1000, task=task)
 dataset = InfiniteDataset(task=task, validation_size=10 ** 4)
