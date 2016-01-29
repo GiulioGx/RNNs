5b66ca05a897026ea4c1c4965fe756198b5a00d5
Giulio <giulio.galvan@gmail.com>
2016-01-29 15:08:35 +0100

    params SEPARATE

--- sources/model/RNNGradient.py
+++ sources/model/RNNGradient.py
@@ -143,10 +143,10 @@ class RNNGradient(SymbolicInfoProducer):
         def __init__(self, gW_rec_list, gW_in_list, gW_out_list, gb_rec_list, gb_out_list, net, l, strategy,
                      preserve_norms=False, grad=None):
 
-            gW_rec_tensor = flatten_list_element(TT.as_tensor_variable(gW_rec_list), l)
-            gW_in_tensor = flatten_list_element(TT.as_tensor_variable(gW_in_list), l)
+            gW_rec_tensor = flatten_list_element(TT.as_tensor_variable(gW_rec_list), l).squeeze()
+            gW_in_tensor = flatten_list_element(TT.as_tensor_variable(gW_in_list), l).squeeze()
             gW_out_tensor = TT.as_tensor_variable(gW_out_list)
-            gb_rec_tensor = flatten_list_element(TT.as_tensor_variable(gb_rec_list), l)
+            gb_rec_tensor = flatten_list_element(TT.as_tensor_variable(gb_rec_list), l).squeeze()
             gb_out_tensor = TT.as_tensor_variable(gb_out_list)
 
             gW_rec_combinantion = strategy.compile(gW_rec_tensor).combination
