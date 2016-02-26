60cdbacd5c3b1fc042d515d8d7a42597577d712a
Giulio <giulio.galvan@gmail.com>
2016-02-26 09:45:51 +0100

    params muse

--- sources/runMuse.py
+++ sources/runMuse.py
@@ -57,11 +57,11 @@ mean = 0
 #     W_out_init=GaussianInit(mean=mean, std_dev=0.1, seed=seed), b_rec_init=ConstantInit(0),
 #     b_out_init=ConstantInit(0))
 vars_initializer = RNNVarsInitializer(
-    W_rec_init=SVDInit(matrix_init=GaussianInit(seed=seed, std_dev=0.14), rho=1.2),
+    W_rec_init=SpectralInit(matrix_init=GaussianInit(seed=seed, std_dev=0.14), rho=1.2),
     W_in_init=GaussianInit(mean=mean, std_dev=0.1, seed=seed),
     W_out_init=GaussianInit(mean=mean, std_dev=0.1, seed=seed), b_rec_init=ConstantInit(0),
     b_out_init=ConstantInit(0))
-net_initializer = RNNInitializer(vars_initializer, n_hidden=200)
+net_initializer = RNNInitializer(vars_initializer, n_hidden=300)
 #net_initializer = RNNLoader(out_dir+'/best_model.npz')
 net_growing_policy = RNNIncrementalGrowing(n_hidden_incr=50, n_hidden_max=300, n_hidden_incr_freq=5000,
                                            initializer=vars_initializer)
@@ -92,21 +92,21 @@ loss_fnc = FullCrossEntropy(single_probability_ouput=True)
 # dir_rule = SepareteGradient()
 
 # combining_rule = OnesCombination(normalize_components=False)
-#combining_rule = SimplexCombination(normalize_components=True, seed=seed)
+combining_rule = SimplexCombination(normalize_components=True, seed=seed)
 # combining_rule = SimpleSum()
 #combining_rule = EquiangularCombination()
 # combining_rule = DropoutCombination(drop_rate=0.8)
 # combining_rule = MedianCombination()
-#dir_rule = CombinedGradients(combining_rule)
+dir_rule = CombinedGradients(combining_rule)
 #dir_rule = DropoutDirection(dir_rule, drop_rate=0.7)
 # dir_rule = DirectionWithPenalty(direction_rule=dir_rule, penalty=penalty, penalty_lambda=1)
 # dir_rule = AlternatingDirections(dir_rule)
 #dir_rule = LBFGSDirection(n_pairs=10)
-dir_rule = Antigradient()
+#dir_rule = Antigradient()
 # learning step rule
 # lr_rule = WRecNormalizedStep(0.0001) #0.01
 # lr_rule = ConstantNormalizedStep(0.001)  # 0.01
-lr_rule = GradientClipping(lr_value=0.01, clip_thr=3, normalize_wrt_dimension=False)  # 0.01
+lr_rule = GradientClipping(lr_value=0.001, clip_thr=3, normalize_wrt_dimension=False)
 # lr_rule = ArmijoStep(alpha=0.5, beta=0.1, init_step=1, max_steps=50)
 
 # update_rule = FixedAveraging(t=10)
