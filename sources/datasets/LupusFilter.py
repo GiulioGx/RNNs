7469d6b4eef14e79c4b7a18bc946037a6ffcc6d5
Giulio <giulio.galvan@gmail.com>
2016-03-22 17:19:05 +0100

    params

--- sources/datasets/LupusFilter.py
+++ sources/datasets/LupusFilter.py
@@ -136,8 +136,8 @@ class TemporalSpanSelector(VisitsSelector):
 
     def __init__(self, min_age_span_upper, min_age_span_lower, min_visits: int = 2):
         self.__min_age_span_upper = float(min_age_span_upper)
-        self.__min_visits = float(min_visits)
-        self.__min_age_span_lower = min_age_span_lower
+        self.__min_visits = min_visits
+        self.__min_age_span_lower = float(min_age_span_lower)
 
     def select_visits(self, visits):
         targets = [v["sdi"].item() for v in visits]
