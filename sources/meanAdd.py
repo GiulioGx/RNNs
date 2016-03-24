cc8ddee66b02f8a20a3e128ee645cd2dbfd0411d
Giulio <giulio.galvan@gmail.com>
2016-03-24 18:03:37 +0100

    ch1 revised

---
+++ sources/meanAdd.py
@@ -0,0 +1,9 @@
+import numpy
+
+run_cheked_val = [1075800, 1075800, 1847000]
+run_cos_vals = [1659000, 3018000]
+run_anti = [1538800, 1853800, 2029800]
+
+print("mean_cos: {}".format(int(numpy.mean(run_cos_vals))))
+print("mean_checked: {}".format(int(numpy.mean(run_cheked_val))))
+print("mean_anti: {}".format(int(numpy.mean(run_anti))))
